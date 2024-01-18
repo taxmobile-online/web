4530bbb33ea4edff1026d7c60017c65eae523f35
Okegbenro Abayomi <okegbenroabayo@gmail.com>
2024-01-18 23:59:49 +0100

    feat: docs viewing

--- src/Components/Pages/ELibrary/ELibrary.tsx
+++ src/Components/Pages/ELibrary/ELibrary.tsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { AdminDashboardChildTemplate } from "Components/Templates/AdminDashboardTemplate";
 import { EmptyCard } from "Components/Molecules/EmptyCard";
 import { CreateMaterialModal } from "Components/Molecules/Modals";
+import useApi from "Utils/Hooks/useApi";
+import endpoints from "Services/endpoints";
 
 // Type defination
 interface Props {}
@@ -10,6 +12,14 @@ interface Props {}
 const ELibrary: React.FC<Props> = () => {
   // States
   const [showModal, setShowModal] = useState<boolean>(false);
+
+  // Hookd
+  const { sendRequest } = useApi();
+
+  // Methods
+  const getDocuments = async () => {
+    await sendRequest("GET", endpoints.getDocumentEndpoint);
+  };
 
   // Data to display
   return (
