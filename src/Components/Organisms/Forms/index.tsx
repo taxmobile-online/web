d0c0ab8bdac5695d4847fba59d992615ea660ab1
Okegbenro Abayomi <okegbenroabayo@gmail.com>
2024-01-30 00:36:22 +0100

    feat: pricing setup

--- src/Components/Organisms/Forms/index.tsx
+++ src/Components/Organisms/Forms/index.tsx
@@ -2,7 +2,7 @@ export { default as SignUpForm } from "./SignUpForm";
 export { default as SignInForm } from "./SignInForm";
 export { default as ResetPasswordForm } from "./ResetPasswordForm";
 export { default as CreateSectionForm } from "./CreateSectionForm";
-export { default as CreateSubSectionForm } from "./CreateSubSectionForm";
 export { default as AdminProfileForm } from "./AdminProfileForm";
 export { default as CreateMaterialForm } from "./CreateMaterialForm";
 export { default as PricingSetupForm } from "./PricingSetupForm";
+export { default as CreateSubSectionForm } from "./CreateSubSectionForm";
