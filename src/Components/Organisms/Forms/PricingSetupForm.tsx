0ed67d2287c0d40e7e4a4210afb0c0a80f248440
Okegbenro Abayomi <okegbenroabayo@gmail.com>
2024-02-05 08:04:43 +0100

    feat: adding pricing setup

--- src/Components/Organisms/Forms/PricingSetupForm.tsx
+++ src/Components/Organisms/Forms/PricingSetupForm.tsx
@@ -19,10 +19,10 @@ import Typography from "Components/Atoms/Typography";
 import { Card, OptionsWrapper } from "./style";
 
 const validationSchema = yup.object().shape({
-  accountType: yup.string().required().min(1).label("Account type"),
   currency: yup.string().required().min(1).label("Currency"),
   price: yup.string().required().min(1).label("Price"),
   subscriberCount: yup.string().required().min(1).label("No of subscriber"),
+  accountType: yup.string().required().min(1).label("Account type"),
 });
 
 // Component
