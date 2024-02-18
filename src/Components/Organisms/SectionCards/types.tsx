3dc57339e994830aec4ee5303053d96c2cc2c516
Okegbenro Abayomi <okegbenroabayo@gmail.com>
2024-02-18 15:06:57 +0100

    refactor: section cards

--- src/Components/Organisms/SectionCards/types.tsx
+++ src/Components/Organisms/SectionCards/types.tsx
@@ -5,6 +5,6 @@ export interface SectionCardsProps extends AllHTMLAttributes<HTMLDivElement> {
   isSub?: boolean;
   sections: Array<{}>;
   isLoading?: boolean;
-  handleFormSuccess?: () => void;
   handleShowModal?: () => void;
+  handleFormSuccess?: () => void;
 }
