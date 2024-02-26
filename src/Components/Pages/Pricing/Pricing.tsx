df07b14abf7035d0f3896e5264c2058594b19b95
Okegbenro Abayomi <okegbenroabayo@gmail.com>
2024-02-26 02:04:24 +0100

    feat: integrating pricing

--- src/Components/Pages/Pricing/Pricing.tsx
+++ src/Components/Pages/Pricing/Pricing.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+
 import { useNavigate } from "react-router-dom";
 import usePageTitle from "Utils/Hooks/usePageTitle";
 import useApi from "Utils/Hooks/useApi";
