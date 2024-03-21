3c18049a257cdda84b6d0b2b86128a0122e3ed75
Okegbenro Abayomi <okegbenroabayo@gmail.com>
2024-03-21 23:40:34 +0100

    refactore: endpoints file

--- src/Services/endpoints.tsx
+++ src/Services/endpoints.tsx
@@ -18,8 +18,8 @@ const getDocumentAccessEndpoint = "/document/token";
 export default {
   // User endpoints
   signInApi,
-  getDocumentAccessEndpoint,
   inviteTeamApi,
+  getDocumentAccessEndpoint,
   adminSignInApi,
   registerUserApi,
   pricingEndpoint,
