bf5e34f9c7b6d57a6d02bbb4c081159ec6c11d05
Okegbenro Abayomi <okegbenroabayo@gmail.com>
2024-03-29 09:28:23 +0100

    refactor:: Analysis page

--- src/Components/Molecules/Analysis/Analysis.tsx
+++ src/Components/Molecules/Analysis/Analysis.tsx
@@ -6,8 +6,8 @@ import {
   Tooltip,
   LineElement,
   PointElement,
-  CategoryScale,
   LinearScale,
+  CategoryScale,
   Chart as ChartJS,
 } from "chart.js";
 import { Line } from "react-chartjs-2";
