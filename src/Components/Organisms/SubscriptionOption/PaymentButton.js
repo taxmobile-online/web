ad2f45a572fac9c904928d217740616acee557b4
Okegbenro Abayomi <okegbenroabayo@gmail.com>
2024-03-30 14:33:26 +0100

    refactor:: Analysis page

--- src/Components/Organisms/SubscriptionOption/PaymentButton.js
+++ src/Components/Organisms/SubscriptionOption/PaymentButton.js
@@ -13,8 +13,8 @@ const PaymentButton = (props) => {
   const { setTransactionId, setAccountType, setPricingId } = useAuthStore();
 
   const config = {
-    reference: new Date().getTime().toString(),
     email: "user@example.com",
+    reference: new Date().getTime().toString(),
     amount: parseInt(`${price}00`),
     publicKey: `pk_test_da2adf82cb1ed414ff60cd5839106a414b6a21bf`,
   };
