5e46db1a3381bd2de91a9bdac13f5d424722421b
Okegbenro Abayomi <okegbenroabayo@gmail.com>
2024-03-30 14:33:14 +0100

    refactor:: Analysis page

--- src/Components/Organisms/SubscriptionOption/PaymentButton.js
+++ src/Components/Organisms/SubscriptionOption/PaymentButton.js
@@ -8,13 +8,14 @@ const PaymentButton = (props) => {
   
   // Props
   const { handleShowModal, price, accountType, btnType, pricingId } = props;
+
   // Store
   const { setTransactionId, setAccountType, setPricingId } = useAuthStore();
 
   const config = {
     reference: new Date().getTime().toString(),
-    email: "user@example.com",
     amount: parseInt(`${price}00`),
+    email: "user@example.com",
     publicKey: `pk_test_da2adf82cb1ed414ff60cd5839106a414b6a21bf`,
   };
   const initializePayment = usePaystackPayment(config);
