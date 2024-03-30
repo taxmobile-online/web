bb7b779eceea37aa6181a08ac3656d7c6016aa5f
Okegbenro Abayomi <okegbenroabayo@gmail.com>
2024-03-30 14:30:14 +0100

    refactor:: Analysis page

--- src/Components/Organisms/SubscriptionOption/index.tsx
+++ src/Components/Organisms/SubscriptionOption/index.tsx
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import { Container, Wrapper } from "./style";
 import Typography from "Components/Atoms/Typography";
 import { PriceSwitch } from "Components/Molecules/PriceSwitch";
-import Plans from "./Plans";
 import { PlanOption } from "Components/Molecules/PlanOption";
+import Plans from "./Plans";
 import { AnimatePresence } from "framer-motion";
 import { RightModal } from "Components/Molecules/Modals";
 
