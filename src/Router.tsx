bd864d7b2d5f6434a5f8528ed5def3f8b280600c
Okegbenro Abayomi <okegbenroabayo@gmail.com>
2024-04-07 02:37:12 +0100

    refactor:: Analysis page

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,9 +6,9 @@ import {
 
 import { Read } from "Components/Pages/Read";
 import { SignIn } from "Components/Pages/SignIn";
-import Home from "Components/Pages/Home";
 import { Library } from "Components/Pages/Library";
 import { Pricing } from "Components/Pages/Pricing";
+import Home from "Components/Pages/Home";
 import { Billing } from "Components/Pages/Billing";
 import { ELibrary } from "Components/Pages/ELibrary";
 import { Sections } from "Components/Pages/Sections";
